e9963a9956ad17b503c4dc9d0df12ae2b7468223
Kelly Keating <kelly.keating.nz@gmail.com>
2025-07-07 11:44:16 +1200

    Comment model file

--- models/index.ts
+++ models/index.ts
@@ -1,3 +1,7 @@
+// -----------------------------------
+// Job Listing Models
+// -----------------------------------
+
 interface JobTemplate {
   id: string
   title: string
@@ -15,22 +19,28 @@ interface JobTemplate {
   notes: string
 }
 
-export interface JobDB extends JobTemplate {
-  bulletPoints: string
-}
-export type JobData = Omit<JobDB, 'saved' | 'applied' | 'hidden' | 'notes'>
-
+/**
+ * @description A job listing with all necessary details for display in the UI.
+ */
 export interface Job extends JobTemplate {
   bulletPoints: string[]
 }
 
-export interface Url {
-  name: string
-  url: string
-  id: number
-  lastFetched: string | null
+/**
+ * @description A job listing as stored in the database, with bullet points stored as a single string.
+ */
+export interface JobDB extends JobTemplate {
+  bulletPoints: string
 }
 
+/**
+ * @description The core job structure before it is saved to the database.
+ */
+export type JobData = Omit<JobDB, 'saved' | 'applied' | 'hidden' | 'notes'>
+
+/**
+ * @description A job listing from Seek, containing only details relevant to the database.
+ */
 export interface SeekJobListing {
   bulletPoints: string[]
   branding: {
@@ -63,13 +73,45 @@ export interface SeekJobListing {
   }
 }
 
+// -----------------------------------
+// URL Models
+// -----------------------------------
+
+/**
+ * @description The URL data structure used to manage job listing URLs.
+ */
+export interface Url {
+  name: string
+  url: string
+  id: number
+  lastFetched: string | null
+}
+
+// -----------------------------------
+// Job Refresh Models
+// -----------------------------------
+
+/**
+ * @description The refresh status during the job fetching process.
+ */
 export type RefreshStatus = 'init' | 'fetching' | 'inserting' | 'completed'
+
+/**
+ * @description The progress data used to display the job fetching status in the UI.
+ */
 export interface ProgressData {
   msg: RefreshStatus
   completedUrls: number
   totalUrls: number
 }
 
+// -----------------------------------
+// Settings Models
+// -----------------------------------
+
+/**
+ * @description Full settings for this application, stored in settings.json file.
+ */
 export type Settings = {
   darkMode: boolean
 }
